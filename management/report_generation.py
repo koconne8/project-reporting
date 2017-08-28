34558fc7dc2eba2682290d91db1835b7fdb88ea5
david-pettifor-nd <dpettifo@nd.edu>
2017-08-28 09:29:02 -0400

    transaction date reverted

--- management/report_generation.py
+++ management/report_generation.py
@@ -408,10 +408,10 @@ def generate_internal_report(request):
                 new_record = {}
                 new_record['name'] = project_name  # Primary Comments
                 new_record['fopal'] = clean_fopal(fopal)  # Customer Account Number
-                new_record['trans'] = str(int(request.GET['month']))+'/'+str(day)+'/'+request.GET['year']
-                # new_record['trans'] = (
-                #     str(day) + '-' + calendar.month_abbr[int(request.GET['month'])].upper() + '-' + request.GET['year'][
-                #                                                                                 2:])  # Transaction Date
+                # new_record['trans'] = str(int(request.GET['month']))+'/'+str(day)+'/'+request.GET['year']
+                new_record['trans'] = (
+                    str(day) + '-' + calendar.month_abbr[int(request.GET['month'])].upper() + '-' + request.GET['year'][
+                                                                                                2:])  # Transaction Date
                 new_record['service'] = cores_display  # (cost_lib.getCORESName(record[3]))		# Service Description
                 new_record['hours'] = record[1]  # Quantity (Hours)
                 new_record['unit'] = 'Hour'  # Unit (hours)
@@ -835,10 +835,10 @@ def generate_external_report(request):
                 new_record = {}
                 new_record['name'] = project_name  # Primary Comments
                 new_record['fopal'] = clean_fopal(fopal)  # Customer Account Number
-                new_record['trans'] = str(int(request.GET['month']))+'/'+str(day)+'/'+request.GET['year']
-                # new_record['trans'] = (
-                #     str(day) + '-' + calendar.month_abbr[int(request.GET['month'])].upper() + '-' + request.GET['year'][
-                #                                                                                 2:])  # Transaction Date
+                # new_record['trans'] = str(int(request.GET['month']))+'/'+str(day)+'/'+request.GET['year']
+                new_record['trans'] = (
+                    str(day) + '-' + calendar.month_abbr[int(request.GET['month'])].upper() + '-' + request.GET['year'][
+                                                                                                2:])  # Transaction Date
                 new_record['service'] = cores_display  # (cost_lib.getCORESName(record[3]))		# Service Description
                 new_record['hours'] = record[0]  # Quantity (Hours)
                 new_record['unit'] = 'Hour'  # Unit (hours)
